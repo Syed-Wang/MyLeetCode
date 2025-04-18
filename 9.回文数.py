15dac0c11830e8ff1af9052f731eb9b3ce993a7d
Syed <wangsy8421@gmail.com>
2025-04-18 22:12:51 +0800

    update

--- 9.回文数.py
+++ 9.回文数.py
@@ -4,6 +4,7 @@
 # [9] 回文数
 #
 
+
 # @lc code=start
 class Solution:
     def isPalindrome(self, x: int) -> bool:
@@ -13,5 +14,20 @@ class Solution:
         return str_x == re_str
         """
 
-# @lc code=end
+        """方法二：数学方法
+        1. 负数不是回文数
+        2. 如果一个数字的最后一位是0，那么它的第一位只能是0
+        3. 反转一半数字，比较反转后的数字和剩下的数字是否相等
+        4. 反转数字的方式：取余和除法
+        """
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+        reversed_num = 0
+        while x > reversed_num:
+            reversed_num = reversed_num * 10 + x % 10
+            x //= 10
+        # 当数字长度为奇数时，可以通过 reversed_num // 10 去除中间的数字
+        return x == reversed_num or x == reversed_num // 10
 
+
+# @lc code=end
